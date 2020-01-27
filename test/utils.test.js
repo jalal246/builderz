1ca4f95238dfaecc98f0d685a5166f2418f90cb2
jalal246 <jimmy002020@gmail.com>
2020-01-27 17:02:59 +0200

    Add test

---
+++ test/utils.test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { getPackagesPath } from "../src/utils/utils";
+
+describe("utils", () => {
+  it("it should work", () => {
+    expect([]).to.deep.equal([]);
+  });
+});
