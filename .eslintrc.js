b408e91ee9a35f76dbc263c2b69fd5208b377009
jalal <jimmy02020@gmail.com>
2020-01-27 17:02:59 +0200

    Add test

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["airbnb-base", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    globals: {
+      describe: true,
+      it: true,
+      expect: true
+    }
+  }
+};
