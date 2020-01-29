16e0fc0c5b80cb4fc456dcae0e60dbfc9904fb94
jalal246 <jimmy002020@gmail.com>
2020-01-29 14:55:13 +0200

    Add watcher and build

---
+++ src/config/rollup.config.js
@@ -0,0 +1,26 @@
+import rollup from "rollup";
+import { error } from "../utils";
+
+async function build(isWatch, onWatch, inputOptions, outputOptions) {
+  try {
+    if (isWatch) {
+      const watcher = rollup.watch({
+        ...inputOptions,
+        output: [outputOptions]
+      });
+      onWatch(watcher);
+    } else {
+      /**
+       * create a bundle
+       */
+      const bundle = await rollup.rollup(inputOptions);
+
+      /**
+       * write the bundle to disk
+       */
+      await bundle.write(outputOptions);
+    }
+  } catch (e) {
+    error(e);
+  }
+}
