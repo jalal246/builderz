c169c722b8cc34b6e937576da5ae92df149d5f91
jalal <jimmy02020@gmail.com>
2020-01-29 16:12:20 +0200

    Add start,  bundlePackage functions to config

--- src/config/rollup.config.js
+++ src/config/rollup.config.js
@@ -1,5 +1,29 @@
 import rollup from "rollup";
-import { error } from "../utils";
+import packageSorter from "package-sorter";
+
+import { UMD, CJS, ES } from "../constants";
+
+import {
+  error,
+  msg,
+  PROD,
+  DEV,
+  getPackagesPath,
+  extractPackagesInfo,
+  cleanBuildDir
+} from "../utils";
+
+import getInput from "./input";
+import getOutput from "./output";
+
+const defaultBundleOpt = [
+  { format: UMD, isProd: false },
+  { format: UMD, isProd: true },
+  { format: CJS, isProd: false },
+  { format: CJS, isProd: true },
+  { format: ES, isProd: false },
+  { format: ES, isProd: true }
+];
 
 async function build(isWatch, onWatch, inputOptions, outputOptions) {
   try {
@@ -24,3 +48,68 @@ async function build(isWatch, onWatch, inputOptions, outputOptions) {
     error(e);
   }
 }
+
+async function bundlePackage({
+  isProd,
+  format,
+  packageName,
+  sourcePath,
+  distPath
+}) {
+  const BUILD_FORMAT = format;
+  const BABEL_ENV = `${isProd ? PROD : DEV}`;
+
+  // babel presets according to env
+  const presets = [];
+  const flags = { BUILD_FORMAT, BABEL_ENV, IS_SILENT: false };
+
+  const input = getInput({
+    sourcePath,
+    presets,
+    flags
+  });
+
+  const output = getOutput({
+    packageName,
+    distPath,
+    flags
+  });
+
+  await build(input, output);
+}
+
+async function start() {
+  // array of packages with paths
+  const allPackages = getPackagesPath();
+
+  // array of packages info according to package.json
+  const packagesInfo = extractPackagesInfo(allPackages);
+
+  // this array will be empty if there's no specfic package is targeted
+  const selectedPackages = [];
+
+  cleanBuildDir({
+    packages: selectedPackages.length === 0 ? allPackages : selectedPackages,
+    filenames: ["dist"]
+  });
+
+  const sorted = packageSorter({ packages: packagesInfo });
+
+  sorted.forEach(pkg => {
+    const { name: packageName, sourcePath, distPath } = pkg;
+
+    defaultBundleOpt.forEach(({ isProd, format }) => {
+      bundlePackage({
+        isProd,
+        format,
+        packageName,
+        sourcePath,
+        distPath
+      });
+    });
+  });
+}
+
+start().catch(err => {
+  error(err);
+});
