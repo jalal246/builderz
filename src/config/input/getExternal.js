13acd7c3d6d4c0eed0eb9df2f73544c0f25c404b
jalal246 <jimmy002020@gmail.com>
2020-02-24 18:26:30 +0200

    Remove getExternal to a different file

---
+++ src/config/input/getExternal.js
@@ -0,0 +1,36 @@
+const { UMD } = require("../../constants");
+
+/**
+ * Resolves external dependencies and peerDependencies for package input
+ * according to build format.
+ *
+ * @param {Object} packageJson
+ * @param {Object} packageJson.peerDependencies
+ * @param {Object} packageJson.dependencies
+ * @param {string} - BUILD_FORMAT
+ *
+ * @returns {function} - function resolver
+ */
+function getExternal({ peerDependencies, dependencies, BUILD_FORMAT }) {
+  const external = [];
+
+  /**
+   * Always exclude peer deps.
+   */
+  if (peerDependencies) {
+    external.push(...Object.keys(peerDependencies));
+  }
+
+  /**
+   * Add dependencies to bundle when umd
+   */
+  if (BUILD_FORMAT !== UMD) {
+    external.push(...Object.keys(dependencies));
+  }
+
+  return external.length === 0
+    ? () => false
+    : id => new RegExp(`^(${external.join("|")})($|/)`).test(id);
+}
+
+module.exports = getExternal;
--- src/config/input/getInput.js
+++ src/config/input/getInput.js
@@ -1,38 +1,5 @@
-const { UMD } = require("../../constants");
 const getPlugins = require("./getInputPlugins");
-
-/**
- * Resolves external dependencies and peerDependencies for package input
- * according to build format.
- *
- * @param {Object} packageJson
- * @param {Object} packageJson.peerDependencies
- * @param {Object} packageJson.dependencies
- * @param {string} - BUILD_FORMAT
- *
- * @returns {function} - function resolver
- */
-function getExternal({ peerDependencies, dependencies, BUILD_FORMAT }) {
-  const external = [];
-
-  /**
-   * Always exclude peer deps.
-   */
-  if (peerDependencies) {
-    external.push(...Object.keys(peerDependencies));
-  }
-
-  /**
-   * Add dependencies to bundle when umd
-   */
-  if (BUILD_FORMAT !== UMD) {
-    external.push(...Object.keys(dependencies));
-  }
-
-  return external.length === 0
-    ? () => false
-    : id => new RegExp(`^(${external.join("|")})($|/)`).test(id);
-}
+const getExternal = require("./getExternal");
 
 /**
  *
@@ -52,7 +19,6 @@ function genInput({
   peerDependencies,
   dependencies,
   sourcePath,
-  presets,
   advancedOpt
 }) {
   const external = getExternal({
@@ -62,7 +28,6 @@ function genInput({
   });
 
   const plugins = getPlugins({
-    presets,
     IS_SILENT,
     BUILD_FORMAT,
     BABEL_ENV,
