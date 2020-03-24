0a3d92fe0fcc224c20e969fb5ea76d3cb9266ae0
jalal246 <jimmy002020@gmail.com>
2020-03-24 21:25:53 +0200

    fix resolveArgs add options and funcs

--- src/resolveArgs.js
+++ src/resolveArgs.js
@@ -1,17 +1,35 @@
-import { option } from "commander";
+import { program } from "commander";
+
+function string2Arr(value) {
+  return value.split(",");
+}
 
 /**
  * Get args pass to build command
  * @return {Object} contains flags and array of packages name
  */
 function resolveArgs() {
-  return option("-s, --silent", "silent mode, mutes build massages")
-    .option("-w, --watch", "watch mode:TODO")
-    .option("-f, --format", "specific build format")
-    .option("-p, --plugins", "input custom plugins")
-    .option("-b, --buildName", "specific build name")
-    .option("-m, --minify", "minify bundle works only if format is provided")
-    .option("PACKAGE_NAME", "building specific package[s], in monorepo")
+  program
+    .option("-s, --silent", "Silent mode, mutes build massages")
+    .option("--format <list>", "Specific build format", string2Arr)
+    .option("-m, --minify", "Minify bundle works only if format is provided")
+    .option("-b, --build-name <string>", "Specific build name", "dist")
+    .option(
+      "--plugins <list>",
+      "Custom plugins works as additional ones",
+      string2Arr
+    )
+    .option(
+      "--paths <list>",
+      "Provide custom paths not in the root/src",
+      string2Arr
+    )
+    .option(
+      "--package-names <list>",
+      "Building specific package[s], in monorepo",
+      string2Arr
+    )
+    .option("-a, --alias <list>", "package alias", string2Arr)
     .parse(process.argv);
 }
 
