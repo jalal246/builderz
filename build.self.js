0dae9c130cee3cfdad9dd1d0fbe2d4d7f5d0fec2
jalal246 <jimmy002020@gmail.com>
2020-04-09 16:32:55 +0200

    Add new option to build

--- build.self.js
+++ build.self.js
@@ -1,14 +1,15 @@
 import builderz from "./src";
 
 [
-  { entries: ["src/cli.js"], output: "cli" },
+  { entries: "src/cli.js", output: "cli", banner: `#!/usr/bin/env node` },
   { entries: "src/builderz.js", output: "builderz" },
-].forEach(({ entries, output }) => {
+].forEach(({ entries, output, banner }) => {
   builderz({
     isSilent: true,
     formats: ["cjs"],
     entries,
     output,
+    banner,
     minify: false,
   });
 });
