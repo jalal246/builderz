a494291cdb75a419dda8afc184e1422a016755f0
jalal246 <jimmy002020@gmail.com>
2020-04-13 21:37:18 +0200

    Remove default for each. Add args so we can parse internal arguments

--- src/resolveArgs.js
+++ src/resolveArgs.js
@@ -1,42 +1,42 @@
 const yargs = require("yargs");
 
-function resolveArgs() {
-  return yargs
+function resolveArgs(args) {
+  yargs
     .option("silent", {
       alias: "s",
       describe: "Silent mode, mutes build massages",
       type: "boolean",
-      default: true,
+      // default: true,
     })
     .option("formats", {
       alias: "f",
       describe: "Specific build format",
       type: "array",
-      default: [],
+      // default: [],
     })
     .option("minify", {
       alias: "m",
       describe: "Minify bundle works only if format is provided",
       type: "boolean",
-      default: false,
+      // default: false,
     })
     .option("camel-case", {
       alias: "c",
       describe: "Add camel-cased output file",
       type: "boolean",
-      default: true,
+      // default: true,
     })
-    .option("camel-case", {
+    .option("clean-build", {
       alias: "l",
       describe: "Clean previous build folder",
       type: "boolean",
-      default: false,
+      // default: false,
     })
     .option("build-name", {
       alias: "b",
       describe: "Specific folder build name",
       type: "string",
-      default: "dist",
+      // default: "dist",
     })
     .option("output", {
       alias: "o",
@@ -47,24 +47,32 @@ function resolveArgs() {
       alias: "w",
       describe: "Provide custom paths not in the root/src",
       type: "array",
-      default: [],
+      // default: [],
+    })
+    .option("pkg-names", {
+      alias: "n",
+      describe: "Building specific package[s], in workspace",
+      type: "array",
+      // default: [],
     })
     .option("alias", {
       alias: "a",
       describe: "Provide custom paths not in the root/srcPackage Alias",
       type: "array",
-      default: [],
+      // default: [],
     })
     .option("entries", {
       alias: "e",
       describe: "Add multi entries instead of default src/index",
-      default: [],
+      // default: [],
     })
     .option("banner", {
       alias: "r",
       describe: "Add banner to output",
       type: "string",
-    }).argv;
+    });
+
+  return args ? yargs.parse(args) : yargs.default().argv;
 }
 
 export default resolveArgs;
