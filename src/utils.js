226d5f54fc457eb81fa4c7982777e7f01b1df54b
jalal246 <jimmy002020@gmail.com>
2020-04-20 23:36:03 +0200

    Add new func utils

--- src/utils.js
+++ src/utils.js
@@ -85,7 +85,25 @@ function getBundleOpt(customFormats, isMinify) {
 
   return gen;
 }
+
+function escapeRegExpCharacters(str) {
+  const regExpCharacters = /[\\^$.*+?()[\]{}|]/g;
+
+  str.replace(regExpCharacters, "\\$&");
+}
+
+function extToRegExp(extensions) {
+  const extReplaced = extensions.map(escapeRegExpCharacters);
+
+  const arrToStrExt = extReplaced.join("|");
+
+  const extRegExp = new RegExp(`(${arrToStrExt})$`);
+
+  return extRegExp;
+}
+
 export {
+  extToRegExp,
   isValidArr,
   NotEmptyArr,
   camelizeOutputBuild,
