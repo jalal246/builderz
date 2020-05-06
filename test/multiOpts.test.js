0df015d6ccc4ab397aaa13d357bd23e5dc5e1720
jalal246 <jimmy002020@gmail.com>
2020-05-07 00:45:18 +0200

    Add test to bundling without package name

--- test/multiOpts.test.js
+++ test/multiOpts.test.js
@@ -3,8 +3,9 @@ import { resolve } from "path";
 import perFile from "./utils";
 
 describe.only.each`
-  pkgName      | desc
-  ${"shebang"} | ${"adding shebang to output file"}
+  pkgName          | desc
+  ${"shebang"}     | ${"adding shebang to output file"}
+  ${"pkg-no-json"} | ${"bundle package doesn't have valid package.json"}
 `("tests $pkgName for testing: $desc", ({ pkgName }) => {
   it("passes", async () => {
     const pathPure = resolve(__dirname, "fixtures", pkgName);
