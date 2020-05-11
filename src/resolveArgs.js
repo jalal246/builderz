25cee4723bf537261e5307217b038fa60d8ced09
jalal246 <jimmy002020@gmail.com>
2020-05-11 19:02:36 +0200

    Add constants to args and restrict aliases to boolean

--- src/resolveArgs.js
+++ src/resolveArgs.js
@@ -1,108 +1,106 @@
+import {
+  SILENT,
+  FORMATS,
+  MINIFY,
+  SOURCE_MAP,
+  CAMEL_CASE,
+  CLEAN_BUILD,
+  BUILD_NAME,
+  OUTPUT,
+  PKG_PATHS,
+  PKG_NAMES,
+  ALIAS,
+  ENTRIES,
+  BANNER,
+  ES_MODEL,
+  STRICT,
+  BABEL,
+  EXTERNAL,
+  SORT_PACKAGES,
+} from "./constants";
+
 const yargs = require("yargs");
 
 function resolveArgs(args) {
   yargs
-    .option("silent", {
+    .option(SILENT, {
       alias: "s",
       describe: "Silent mode, mutes build massages",
       type: "boolean",
-      // default: true,
     })
-    .option("formats", {
-      alias: "f",
-      describe: "Specific build format",
-      type: "array",
-      // default: [],
-    })
-    .option("minify", {
+    .option(MINIFY, {
       alias: "m",
       describe: "Minify bundle works only if format is provided",
       type: "boolean",
-      // default: false,
     })
-    .option("sourcemap", {
+    .option(SOURCE_MAP, {
+      alias: "p",
       describe: "Enable sourcemap in output",
       type: "boolean",
-      // default: true,
     })
-    .option("camel-case", {
+    .option(CAMEL_CASE, {
       alias: "c",
       describe: "Add camel-cased output file",
       type: "boolean",
-      // default: true,
     })
-    .option("clean-build", {
+    .option(CLEAN_BUILD, {
       alias: "l",
       describe: "Clean previous build folder",
       type: "boolean",
-      // default: false,
     })
-    .option("build-name", {
-      alias: "b",
-      describe: "Specific folder build name",
-      type: "string",
-      // default: "dist",
-    })
-    .option("output", {
-      alias: "o",
-      describe: "Custom output name",
-      type: "string",
-    })
-    .option("pkg-paths", {
-      alias: "w",
-      describe: "Provide custom paths not in the root/src",
-      type: "array",
-      // default: [],
-    })
-    .option("pkg-names", {
-      alias: "n",
-      describe: "Building specific package[s], in workspace",
-      type: "array",
-      // default: [],
-    })
-    .option("alias", {
-      alias: "a",
-      describe: "Provide custom paths not in the root/srcPackage Alias",
-      type: "array",
-      // default: [],
-    })
-    .option("entries", {
-      alias: "e",
-      describe: "Add multi entries instead of default src/index",
-      // default: [],
-    })
-    .option("banner", {
-      alias: "r",
-      describe: "Add banner to output",
-      type: "string",
-    })
-    .option("es-module", {
-      alias: "d",
-      describe: "Define Property exports",
-      type: "boolean",
-      // default: [],
-    })
-    .option("strict", {
+    .option(STRICT, {
       alias: "t",
       describe: "Enable Strict Mode",
       type: "boolean",
-      // default: [],
     })
-    .option("babel", {
+    .option(SORT_PACKAGES, {
+      alias: "r",
+      describe: "Enable sorting packages for monorepo",
+      type: "boolean",
+    })
+    .option(ES_MODEL, {
+      alias: "d",
+      describe: "Define Property exports es_model",
+      type: "boolean",
+    })
+    .option(FORMATS, {
+      describe: "Specific build format",
+      type: "array",
+    })
+    .option(BUILD_NAME, {
+      describe: "Specific folder build name",
+      type: "string",
+    })
+    .option(OUTPUT, {
+      describe: "Custom output name",
+      type: "string",
+    })
+    .option(PKG_PATHS, {
+      describe: "Provide custom paths not in the root/src",
+      type: "array",
+    })
+    .option(PKG_NAMES, {
+      describe: "Building specific package[s], in workspace",
+      type: "array",
+    })
+    .option(ALIAS, {
+      describe: "Provide custom paths not in the root/srcPackage Alias",
+      type: "array",
+    })
+    .option(ENTRIES, {
+      describe: "Add multi entries instead of default src/index",
+    })
+    .option(BANNER, {
+      describe: "Add banner to output",
+      type: "string",
+    })
+    .option(BABEL, {
       describe:
         "properties: enablePreset / enablePlugins / extensions / exclude",
-      // type: "boolean",
-      // default: [],
     })
-    .option("external", {
+    .option(EXTERNAL, {
       describe: "Passing external libraries not to bundle",
-      // type: "boolean",
-      // default: [],
-    })
-    .option("sort-pkg", {
-      describe: "Enable sorting packages for monorepo",
-      // type: "boolean",
-      // default: [],
+      type: "array",
     });
 
   return args ? yargs.parse(args) : yargs.argv;
